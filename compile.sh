f0cec01acfe952a34c4166ea80fbd6d262f826fa
bob <69279611+abou704@users.noreply.github.com>
2022-02-08 12:08:16 +0100

    update hadoop

---
+++ compile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# compile hadoop 2.7.2 by default
+version=${1:-2.7.2}
+
+cd /hadoop-$version-src
+
+echo -e "\n\ncomile hadoop $version..."
+mvn package -Pdist,native -DskipTests -Dtar
+
+if [[ $? -eq 0 ]]; then
+	echo -e "\n\ncomile hadoop $version success!\n\n"
+	#mv ./hadoop-dist/target/hadoop-$version.tar.gz /binary
+else
+        echo -e "\n\ncomile hadoop $version fail!\n\n"
+fi
+
+
